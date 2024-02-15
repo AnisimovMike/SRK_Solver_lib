614222f3421a96b998558409e8f12edd53c5a8d2
anisimov <m0534171@gmail.com>
2024-02-15 18:29:11 +0300

    make setup files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='SRK_Solver_lib',                    # package name
     version='0.1',                          # version
     description='Package Description will be later',      # short description
-    url='',               # package URL
+    url='https://github.com/AnisimovMike/SRK_Solver_lib',               # package URL
     install_requires=[],                    # list of packages this package depends
                                             # on.
     packages=['SRK_Solver_lib'],              # List of module names that installing
