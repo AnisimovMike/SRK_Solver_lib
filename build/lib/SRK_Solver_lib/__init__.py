0ffa16a411b21dd1e8ec371e026f77a61ba246d7
anisimov <m0534171@gmail.com>
2024-03-25 21:34:26 +0300

    returning data in dataframe format

--- build/lib/SRK_Solver_lib/__init__.py
+++ build/lib/SRK_Solver_lib/__init__.py
@@ -1,4 +1,5 @@
 from ctypes import *
+import pandas as pd
 
 lib = cdll.LoadLibrary(r"DLL1.dll")
 
@@ -6,5 +7,24 @@ lib = cdll.LoadLibrary(r"DLL1.dll")
 def levy_flights(dt_sav, t, alpha, dt, eta_a, tau_e):
     result = lib.Levy_Flights(c_double(dt_sav), c_int(t), c_double(alpha), c_double(dt), c_double(eta_a),
                               c_double(tau_e))
-    print('Результат моделирования в файле model_Levy_Flights.data')
-    return result
+    print('Результат моделирования сохранены в файле model_Levy_Flights.data')
+    file = open('model_Levy_Flights.data', 'r')
+    t = []
+    X = []
+    Y = []
+    f = 0
+    for line in file:
+        if f == 0:
+            f = 1
+        else:
+            temp = line.split(';')
+            t.append(temp[0])
+            X.append(temp[1])
+            Y.append(temp[2])
+    df = pd.DataFrame({"time": t,
+                       "X[1]": X,
+                       "Y[1]": Y})
+    df['time'] = df['time'].astype(float)
+    df['X[1]'] = df['X[1]'].astype(float)
+    df['Y[1]'] = df['Y[1]'].astype(float)
+    return df
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='0.1',                          # version
     description='Package Description will be later',      # short description
     url='https://github.com/AnisimovMike/SRK_Solver_lib',               # package URL
-    install_requires=[],                    # list of packages this package depends
+    install_requires=['pandas>=2.2.1'],                    # list of packages this package depends
                                             # on.
     packages=['SRK_Solver_lib'],              # List of module names that installing
                                             # this package will provide.
