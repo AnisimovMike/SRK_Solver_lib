97600b6c7b34859f89df9697249a3dbc15c7bbde
anisimov <m0534171@gmail.com>
2024-04-02 13:20:35 +0300

    version 0.1

--- build/lib/SRK_Solver_lib/__init__.py
+++ build/lib/SRK_Solver_lib/__init__.py
@@ -5,10 +5,40 @@ lib = cdll.LoadLibrary(r"DLL1.dll")
 
 
 def levy_flights(dt_sav, t, alpha, dt, eta_a, tau_e):
-    result = lib.Levy_Flights(c_double(dt_sav), c_int(t), c_double(alpha), c_double(dt), c_double(eta_a),
-                              c_double(tau_e))
+    print('Начал моделирование')
+    result = lib.Levy_Flights(c_double(dt_sav), c_int(t), c_double(alpha),
+                              c_double(dt), c_double(eta_a), c_double(tau_e))
     print('Результат моделирования сохранены в файле model_Levy_Flights.data')
     file = open('model_Levy_Flights.data', 'r')
+    t = []
+    X = []
+    Y = []
+    f = 0
+    for line in file:
+        if f == 0:
+            f = 1
+        else:
+            temp = line.split(';')
+            t.append(temp[0])
+            X.append(temp[1])
+            Y.append(temp[2])
+    df = pd.DataFrame({"time": t,
+                       "X[1]": X,
+                       "Y[1]": Y})
+    df['time'] = df['time'].astype(float)
+    df['X[1]'] = df['X[1]'].astype(float)
+    df['Y[1]'] = df['Y[1]'].astype(float)
+    return df
+
+
+def Solver(file_name, py_par, F, N, n):
+    arr_c_par = (c_double * len(py_par))(*py_par)
+    print('Начал моделирование')
+    result = lib.Solver(file_name.encode('utf-8'), arr_c_par, F.encode('utf-8'),
+                        N.encode('utf-8'), c_int(n), c_int(len(F)),
+                        c_int(len(N)), c_int(len(file_name)))
+    print(f'Результат моделирования сохранены в файле {file_name}')
+    file = open(file_name, 'r')
     t = []
     X = []
     Y = []
--- setup.py
+++ setup.py
@@ -3,11 +3,9 @@ from setuptools import setup
 
 setup(
     name='SRK_Solver_lib',                    # package name
-    version='0.1',                          # version
-    description='Package Description will be later',      # short description
-    url='https://github.com/AnisimovMike/SRK_Solver_lib',               # package URL
-    install_requires=['pandas>=2.2.1'],                    # list of packages this package depends
-                                            # on.
-    packages=['SRK_Solver_lib'],              # List of module names that installing
-                                            # this package will provide.
+    version='0.1',                            # version
+    description='Solving stochastic differential equations using the Runge Kutta method',      # short description
+    url='https://github.com/AnisimovMike/SRK_Solver_lib',                                      # package URL
+    install_requires=['pandas>=2.2.1'],                    # list of packages this package depends on.
+    packages=['SRK_Solver_lib'],              # List of module names that installing this package will provide.
 )
